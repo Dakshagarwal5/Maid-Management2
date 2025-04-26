582b924c17796bbfaa9877919c39a34bd0855e2f
Daksh <agarwaldaksh360@gmail.com>
2025-04-26 14:08:11 +0530

    Fixed view engine setup for Render hosting

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const maidRoutes = require('./routes/maidRoutes');
-const connectDB = require('./config/db'); // MongoDB connection
+const connectDB = require('./config/db');
 require('dotenv').config();
 
 const app = express();
@@ -10,8 +10,9 @@ const app = express();
 // Connect to MongoDB
 connectDB();
 
-// View Engine
+// View Engine Setup
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views')); // <-- THIS LINE IS IMPORTANT
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -19,7 +20,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Routes
-app.get('/', (req, res) => res.render('home')); // Homepage
+app.get('/', (req, res) => res.render('home')); // Home page -> views/home.ejs
 app.use('/maids', maidRoutes);
 
 // Server
