cd12da334df267d083add788dbb3e928512fdda4
Daksh <agarwaldaksh360@gmail.com>
2025-04-26 14:45:36 +0530

    Fixed view engine setup for Render hosting

--- app.js
+++ app.js
@@ -10,19 +10,21 @@ const app = express();
 // Connect to MongoDB
 connectDB();
 
-// View Engine Setup
+// View Engine
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views')); // <-- THIS LINE IS IMPORTANT
+app.set('views', path.join(__dirname, 'views'));
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// Routes
+app.get('/', (req, res) => res.render('home'));
+app.use('/maids', maidRoutes);
+
+// Static files (should be AFTER routes)
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-// Routes
-app.get('/', (req, res) => res.render('home')); // Home page -> views/home.ejs
-app.use('/maids', maidRoutes);
-
 // Server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}/maids`));
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
